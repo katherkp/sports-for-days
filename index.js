f5f0f93106ac4519eeea590c5efc00d4497c9f04
Katherine Pham <katherinepham@Katherines-MacBook-Pro-2.local>
2022-12-01 18:36:06 -0800

    setting up routes

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("We are the best team!");
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    
+})
+
+app.listen(process.env.PORT)
